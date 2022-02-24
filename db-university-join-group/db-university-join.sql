f91e556cea2d8bcfffb8ae6e900b932272f4fa7a
Cezar <cezarlantosgmail.com>
2022-02-24 11:16:10 +0100

    fixed join.sql

--- db-university-join-group/db-university-join.sql
+++ db-university-join-group/db-university-join.sql
@@ -3,14 +3,14 @@ SELECT `students`.`name`, `students`.`surname`, `students`.`id`, `degrees`.`name
 FROM `students`
 JOIN `degrees`
 	ON `students`.`degree_id` = `degrees`.`id`
-WHERE `degrees`.`name` LIKE 'Corso di Laurea in Economia';
+WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 
 --2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 SELECT * 
 FROM `departments`
 JOIN `degrees`
 	ON `departments`.`id` = `degrees`.`department_id`
-WHERE `departments`.`name` LIKE 'Dipartimento di Neuroscienze';
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
 
 --3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 SELECT `teachers`.`name`, `teachers`.`surname`, `courses`.`id`, `courses`.`name`, 
